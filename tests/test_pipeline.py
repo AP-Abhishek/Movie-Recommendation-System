c4d7d893e9ca82e74541766a0d8951743bc466d0
AP-Abhishek <abhishekap12005@gmail.com>
2026-01-25 16:18:05 +0530

    feat: updated tests

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,6 +1,8 @@
 import os
+import joblib
 import pandas as pd
 from src.preprocessing import clean_data
+from src.recommender import build_similarity_matrix
 
 def test_raw_files_exists():
     assert os.path.exists('data/raw/tmdb_5000_movies.csv')
@@ -25,3 +27,29 @@ def test_clean_data_output():
 
 def test_processed_file_exists():
     assert os.path.exists('data/processed/movies.csv')
+
+def test_model_generation():
+    build_similarity_matrix()
+    assert os.path.exists('models/similarity_matrix.joblib')
+
+    matrix = joblib.load('models/similarity_matrix.joblib')
+    df = pd.read_csv('data/processed/movies.csv')
+
+    assert matrix.shape[0] == matrix.shape[1]
+    assert matrix.shape[0] == len(df)
+
+def test_recommendation_output():
+    df = pd.read_csv('data/processed/movies_list.csv')
+    similarity = joblib.load('models/similarity_matrix.joblib')
+
+    movie_title = 'Avatar'
+    movie_index = df[df['title'] == movie_title].index[0]
+
+    distances = similarity[movie_index]
+
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_titles = [df.iloc[i[0]].title for i in movies_list]
+
+    assert len(recommended_titles) == 5
+    assert isinstance(recommended_titles[0], str)
